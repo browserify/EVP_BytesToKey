67bedf1e894f0e8c50335d24c1ccd7382a3e64e3
Daniel Cousens <github@dcousens.com>
2017-08-21 12:57:53 +1000

    restore v1.0.0 API

--- test/index.js
+++ test/index.js
@@ -12,39 +12,31 @@ var crypto = require('crypto')
 var test = require('tape')
 var EVP_BytesToKey = require('../')
 
-function createHashMD5 () { return crypto.createHash('md5') }
 var keyLen = 32
 var ivLen = 16
 
-var counts = [1, 2, 100]
-for (var i = 0; i < counts.length; ++i) {
-  var count = counts[i]
+for (var i = 0; i < 1000; ++i) {
+  var password = crypto.randomBytes(10 + Math.round(Math.random() * 100))
+  var salt = crypto.randomBytes(8)
 
-  test('count = ' + count, function (t) {
-    var salt = crypto.randomBytes(8)
-    var data = crypto.randomBytes(10 + Math.round(Math.random() * 100))
+  test('salt is null', function (t) {
+    var result = EVP_BytesToKey(password, null, keyLen, ivLen)
+    var expected = OpenSSL_EVP_BytesToKey.md5_key32_iv16(null, password, 1)
+    t.same(result, expected)
+    t.end()
+  })
 
-    t.test('salt is null', function (t) {
-      var result = EVP_BytesToKey(createHashMD5, null, data, count, keyLen, ivLen)
-      var expected = OpenSSL_EVP_BytesToKey.md5_key32_iv16(null, data, count)
-      t.same(result, expected)
-      t.end()
-    })
-
-    t.test('salt is not null', function (t) {
-      var result = EVP_BytesToKey(createHashMD5, salt, data, count, keyLen, ivLen)
-      var expected = OpenSSL_EVP_BytesToKey.md5_key32_iv16(salt, data, count)
-      t.same(result, expected)
-      t.end()
-    })
-
+  test('salt is not null', function (t) {
+    var result = EVP_BytesToKey(password, salt, keyLen, ivLen)
+    var expected = OpenSSL_EVP_BytesToKey.md5_key32_iv16(salt, password, 1)
+    t.same(result, expected)
     t.end()
   })
 }
 
 test('salt buffer length is 7', function (t) {
   t.throws(function () {
-    EVP_BytesToKey(createHashMD5, Buffer.alloc(7))
+    EVP_BytesToKey(Buffer.alloc(5), Buffer.alloc(7))
   }, /^RangeError: salt should be Buffer with 8 byte length$/)
   t.end()
 })
